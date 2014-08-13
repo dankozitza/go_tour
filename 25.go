30345c3b6687a7e055b32d1fa7c2bfa9e3380c59
dan kozitza <dankoz@gmx.us>
2014-08-13 01:36:41 -0500

    messing with loops/math

--- 25.go
+++ 25.go
@@ -5,8 +5,32 @@ import (
 )
 
 func Sqrt(x float64) float64 {
+   var z float64 = 1
+   var last float64 = 0
+   var loops int = 1
+   for {
+      var diff float64 = last - z
+      if diff < 0 {
+         diff = diff * -1;
+      }
+
+      if diff > .01 {
+         last = z
+         z = z - (((x * x) - x) / (2 * x))
+         loops = loops + 1
+         fmt.Println(z)
+      } else {
+         break
+      }
+   }
+
+   fmt.Println("loops: ")
+   fmt.Println(loops)
+
+   return z
 }
 
 func main() {
-    fmt.Println(Sqrt(2))
+    fmt.Println(Sqrt(4))
+    fmt.Println("\n")
 }
