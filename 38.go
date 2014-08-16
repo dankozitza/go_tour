79ef12507bb62ab5af882af7548fabc53d7881b7
dan kozitza <dankoz@gmx.us>
2014-08-16 11:28:54 -0500

    working on Pic

--- 38.go
+++ 38.go
@@ -8,7 +8,18 @@ import (
 func Pic(dx, dy int) [][]uint8 {
    //var graph [][]uint8
 
-   graph := [][]uint8{{1, 2, 3}, {1, 2, 3}, {1, 2, 3},}
+   //xgraph := make(uint8, dx)
+   graph := make([][]uint8, dy)
+
+   for y := range graph {
+      graph[y] = make([]uint8, dx)
+      //fmt.Println(, "\n")
+      for x := range graph[y] {
+         graph[y][x] = uint8(x) * uint8(y)
+      }
+   }
+
+   //graph := [][]uint8{{1, 2, 3}, {1, 2, 3}, {1, 2, 3},}
    fmt.Println(graph, len(graph), cap(graph))
    //for i := 0; i < 10; i++ {
    //   var pix uint8 = 1
