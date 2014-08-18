d7da4988ce976acc65dafc6a8e2189e2582ded72
dan kozitza <dankoz@gmx.us>
2014-08-18 10:21:14 -0500

    working on 63

--- 63.go
+++ 63.go
@@ -3,10 +3,12 @@ package main
 import (
    "code.google.com/p/go-tour/pic"
    "image"
-   //"image/color"
+   "image/color"
 )
 
-type Image struct{}
+type Image struct{
+   graph [][]color.Color
+}
 
 func (i Image) ColorModel() color.Model {
    return color.RGBAModel
@@ -19,16 +21,29 @@ func (i Image) Bounds() image.Rectangle {
    //r.Min.Y = 0
    //r.Max.Y = 9
 
-   r := image.Rect(0, 0, 10, 10)
+   r := image.Rect(0, 0, 255, 255)
 
    return r
 }
 
 func (i Image) At(x, y int) color.Color {
-   
+   return i.graph[y][x]
+}
+
+func generate_colors(img *Image) {
+ 
+    img.graph := make([][]img.colorModel, img.Bounds.Max.Y)
+ 
+    for y := range img.graph {
+       img.graph[y] = make([]img.colorModel, img.Bounds.Max.X)
+       for x := range graph[y] {
+          img.graph[y][x] = img.colorModel{x, y, 255, 255}
+       }
+    }
 }
 
 func main() {
     m := Image{}
+    generate_colors(m)
     pic.ShowImage(m)
 }
