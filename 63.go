1dae36ddbf756d086d9b1f25577d9b998ed0b51e
dan kozitza <dankoz@gmx.us>
2014-08-28 05:53:31 -0500

    added rotby

--- 63.go
+++ 63.go
@@ -11,11 +11,7 @@ type rot13Reader struct {
 	letters []byte
 }
 
-type rotReader struct {
-	r       io.Reader
-	letters []byte
-	rotby   int
-}
+
 
 func (r13 *rot13Reader) Read(p []byte) (n int, e error) {
 
@@ -51,10 +47,53 @@ func (r13 *rot13Reader) Read(p []byte) (n int, e error) {
 	return
 }
 
+type rotReader struct {
+	r       io.Reader
+	letters []byte
+	rotby   int
+}
+
+func (rn *rotReader) Read(p []byte) (n int, e error) {
+
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	n, e = rn.r.Read(p)
+
+	l_map := make(map[byte]int, len(p)+1)
+
+	for i, l := range rn.letters {
+		//fmt.Println("letter: [", string(l), "], index: [", i, "]")
+		l_map[l] = i
+	}
+
+	for i, l := range p {
+		offset, ok := l_map[l]
+		if ok {
+
+			offset += rn.rotby
+
+			if offset >= len(rn.letters) {
+				offset -= len(rn.letters)
+			}
+
+			//fmt.Println("position: [", l_map[l], "], offset: [", offset, "], i: [", i, "], l: [", string(l), "]")
+
+			p[i] = rn.letters[offset]
+		}
+	}
+
+	return
+}
+
 func main() {
 	s := strings.NewReader(
 		"lbh penxrq gur pbqr!\n")
-	r := rot13Reader{s, []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}}
+	l := []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 
-	io.Copy(os.Stdout, &r)
+	//r := rot13Reader{s, l}
+	rn := rotReader{s, l, 13}
+
+	io.Copy(os.Stdout, &rn)
 }
