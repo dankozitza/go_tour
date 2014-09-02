57345d970bea2fc0953f04969aeff68f616b5ac5
dan kozitza <dankoz@gmx.us>
2014-09-02 10:51:20 -0500

    working on 72

--- 72.go
+++ 72.go
@@ -1,14 +1,31 @@
 package main
 
-import "code.google.com/p/go-tour/tree"
+import (
+	"fmt"
+	"code.google.com/p/go-tour/tree"
+)
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func Walk(t *tree.Tree, ch chan int) {
+	for {
+		select {
+		case c <- 
+}
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+//func Same(t1, t2 *tree.Tree) bool
 
 func main() {
+
+	ftree := tree.New(1)
+	fmt.Println(ftree.String())
+
+	fchan := make(chan int)
+
+	stree := tree.New(1)
+	fmt.Println(stree.String())
+
+	schan := make(chan int)
 }
