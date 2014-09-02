0e35878c7f33411439e340bd032da24afb7eacf3
dan kozitza <dankoz@gmx.us>
2014-09-02 11:22:43 -0500

    getting closer

--- 72.go
+++ 72.go
@@ -8,24 +8,36 @@ import (
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	for {
-		select {
-		case c <- 
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-//func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	Walk(t1, c1)
+	Walk(t2, c2)
+
+	for {
+		fmt.Println(<-c1, " ", <-c2)
+	}
+}
 
 func main() {
 
 	ftree := tree.New(1)
 	fmt.Println(ftree.String())
 
-	fchan := make(chan int)
-
 	stree := tree.New(1)
 	fmt.Println(stree.String())
 
-	schan := make(chan int)
+	fmt.Println(Same(ftree, stree))
 }
